b6d7d9bde3ba6c07a364a0c6f2cb62d4754c6015
Ricardo Parra <ricardo.pxr16@gmail.com>
2024-03-30 23:00:58 -0400

    fix: columna id_sede  agregada en la vw de producto_sede

--- bin/config/SQL/wesley_views.sql
+++ bin/config/SQL/wesley_views.sql
@@ -1,6 +1,6 @@
 -- VISTA PARA PRODUCTO_SEDE CON TODAS LAS RELACIONES
 CREATE OR REPLACE VIEW vw_producto_sede_detallado  AS 
-SELECT ps.id_producto_sede, concat(tp.nombrepro,' ',pres.peso,' ',med.nombre,' ') AS presentacion_producto, tp.nombrepro as nombre_producto, pres.peso as presentacion_peso, med.nombre as medida, ps.lote, ps.fecha_vencimiento, ps.cantidad, s.nombre, tipo.nombre_t as tipo, clase.nombre_c as clase
+SELECT ps.id_producto_sede, concat(tp.nombrepro,' ',pres.peso,' ',med.nombre,' ') AS presentacion_producto, tp.nombrepro as nombre_producto, pres.peso as presentacion_peso, med.nombre as medida, ps.lote, ps.fecha_vencimiento, ps.cantidad, s.nombre, tipo.nombre_t as tipo, clase.nombre_c as clase, s.id_sede
 FROM producto_sede ps 
 INNER JOIN producto p ON p.cod_producto = ps.cod_producto
 INNER JOIN tipo_producto tp ON tp.id_tipoprod = p.id_tipoprod
