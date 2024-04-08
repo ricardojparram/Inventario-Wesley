85d92c6cf18ad940bd005b6ca02afb0b14453518
Ricardo Parra <ricardo.pxr16@gmail.com>
2024-04-07 22:36:22 -0400

    refactor: cambios en el manejo de errores
    Se utilizan nuevas funciones para el manejo de errores y ademas se actualizaron las funciones para validar contrase;a y correo.

--- assets/js/perfil.js
+++ assets/js/perfil.js
@@ -52,14 +52,16 @@ $(document).ready(function () {
 		let valid = validarCedula($("#cedulaEdit"), $("#errorCedu"), "Error de cedula,", $("#preDocument"))
 		clearTimeout(timeout)
 		timeout = setTimeout(function () {
-			if (valid) { validarC($("#cedulaEdit"), $("#errorCedu"), $("#preDocument")) }
+			if (valid) { validarCedulaBD($("#cedulaEdit"), $("#errorCedu"), $("#preDocument")) }
 		}, 700)
 	});
 	$("#emailEdit").keyup(() => {
 		let valid = validarCorreo($("#emailEdit"), $("#errorEmail"), "Error de email,")
 		clearTimeout(timeout)
 		timeout = setTimeout(function () {
-			if (valid) { validarE($("#emailEdit"), $("#errorEmail")) }
+			if (valid) {
+				console.log(validarCorreoBD($("#emailEdit"), $("#errorEmail")))
+			}
 		}, 700)
 	});
 	let name, lastname, id, email;
@@ -138,26 +140,22 @@ $(document).ready(function () {
 			throw new Error('Datos inválidos');
 		}
 
-		validarC($("#cedulaEdit"), $("#errorCedu"), $("#preDocument")).then(() => {
+		validarCedulaBD($("#cedulaEdit"), $("#errorCedu"), $("#preDocument")).then(() => {
 
-
-			validarE($("#emailEdit"), $("#errorEmail")).then(() => {
-
+			validarCorreoBD($("#emailEdit"), $("#errorEmail")).then(() => {
 
 				let form = new FormData($('#formEditar')[0]);
 				form.append("cedula", $("#preDocument").val() + "-" + $("#cedulaEdit").val());
 				let borrar = $('#imgEditar').is('[src="assets/img/profile_photo.jpg"]');
+				let foto = $('#foto').val();
 
-				if (borrar != true) {
-					if (typeof canvas === "undefined" || typeof canvas == null) {
-						Toast.fire({ icon: 'warning', title: 'No ha cambiado la imagen.' });
-						throw Error('Canvas no tiene ninguna imagen cortada');
-					} else {
-						canvas.toBlob(function (blob) {
-							form.set('foto', blob, 'avatar.png')
-							editarImagen(form);
-						});
-					}
+				if (foto !== "") {
+					canvas.toBlob(function (blob) {
+						form.set('foto', blob, 'avatar.png')
+						editarImagen(form);
+					});
+				} else {
+					editarImagen(form);
 				}
 
 				if (borrar) {
@@ -172,40 +170,8 @@ $(document).ready(function () {
 	})
 	function editarImagen(form) {
 		$.ajax({
-			type: "POST",
-			url: '',
-			dataType: 'JSON',
-			data: form,
-			contentType: false,
-			processData: false,
-			xhr() {
-				let xhr = new window.XMLHttpRequest();
-				$('#displayProgreso').show();
-				xhr.upload.addEventListener("progress", function (event) {
-
-					if (event.lengthComputable) {
-						let porcentaje = parseInt((event.loaded / event.total * 100), 10);
-						$('#progressBar').data("aria-valuenow", porcentaje)
-						$('#progressBar').css("width", porcentaje + '%')
-						$('#progressBar').html(porcentaje + '%')
-					}
-
-				}, false)
-				xhr.addEventListener("progress", function (e) {
-
-					if (e.lengthComputable) {
-						percentComplete = parseInt((e.loaded / e.total * 100), 10);
-						$('#progressBar').data("aria-valuenow", percentComplete);
-						$('#progressBar').css("width", percentComplete + '%');
-						$('#progressBar').html(percentComplete + '%');
-					} else {
-						$('#progressBar').html("Upload");
-					}
-
-				}, false);
-
-				return xhr;
-			},
+			type: "POST", url: '', dataType: 'JSON', data: form, contentType: false, processData: false,
+			xhr: () => loading(),
 			success(data) {
 				$('#displayProgreso').hide();
 				if (data.foto.respuesta == 'Error') {
@@ -233,112 +199,100 @@ $(document).ready(function () {
 		})
 	}
 
-	let actContra;
-	function validarContra(input, div) {
+	async function validarContraseñaBD(input, div) {
 		let password = input.val();
-		$.post('', { password, validarContraseña: 'asd' },
-			function (response) {
-				data = JSON.parse(response);
-				if (data.resultado == 'Error de contraseña') {
-					actContra = false;
-					div.text(data.error);
-					input.attr("style", "border-color: red; background-image: url(assets/img/Triangulo_exclamacion.png); background-repeat: no-repeat; background-position: right calc(0.375em + 0.1875rem) center; background-size: calc(0.75em + 0.375rem) calc(0.75em + 0.375rem);");
-				} else {
-					actContra = true;
-				}
-			});
+		let resultado = false;
+		await $.post('', { password, validarContraseña: '' }, function (response) {
+			div.text(" ");
+			input.removeClass('input-error');
+			resultado = true;
+		}, "json").fail(e => {
+			div.text(e.responseJSON.msg);
+			input.addClass('input-error')
+			resultado = false;
+		})
+		return resultado;
 	}
 
 	$("#password").keyup(() => {
 		let valid = validarContraseña($("#password"), $("#error2"), "Error de Contraseña Actual,")
 		clearTimeout(timeout);
 		timeout = setTimeout(function () {
-			if (valid) validarContra($("#password"), $("#error2"));
+			if (valid) validarContraseñaBD($("#password"), $("#error2"));
 		}, 700);
 	});
 	$("#newPassword").keyup(() => { validarContraseña($("#newPassword"), $("#error2"), "Error de Contraseña Nueva,") });
 	$("#rePassword").keyup(() => { validarRepContraseña($("#rePassword"), $("#error2"), $("#newPassword")) });
 	let contra, reContra;
-	$("#editContra").click((e) => {
+	$("#editContra").click(function (e) {
 		e.preventDefault()
-		if (click >= 1) throw new Error('Spam de clicks');
 		reContra = validarRepContraseña($("#rePassword"), $("#error2"), $("#newPassword"));
 		contra = validarContraseña($("#newPassword"), $("#error2"), "Error de Contraseña Nueva,");
 
-		if (actContra && contra && reContra) {
-			$.ajax({
-				url: '',
-				method: 'post',
-				dataType: 'JSON',
-				data: {
-					passwordAct: $("#password").val(),
-					passwordNew: $("#newPassword").val(),
-					passwordNewR: $("#rePassword").val()
-				}, success(des) {
-					// console.log(des);
-					if (des.resultado === 'Error de contraseña') {
-						$("#error2").text(des.error);
-						$("#password").attr("style", "border-color: red; background-image: url(assets/img/Triangulo_exclamacion.png); background-repeat: no-repeat; background-position: right calc(0.375em + 0.1875rem) center; background-size: calc(0.75em + 0.375rem) calc(0.75em + 0.375rem);");
-					}
-					if (des.resultado === 'Error de repetida') {
-						$("#error2").text(des.error);
-						$("#newPassword").attr("style", "border-color: red; background-image: url(assets/img/Triangulo_exclamacion.png); background-repeat: no-repeat; background-position: right calc(0.375em + 0.1875rem) center; background-size: calc(0.75em + 0.375rem) calc(0.75em + 0.375rem);");
-					}
-					if (des.resultado === 'Editada Contraseña') {
-						Toast.fire({ icon: 'success', title: 'Contraseña Actualizada' });
-						$("#perfil").click();
-						$("#profile-change-password input").val("")
-					}
-				}
-			})
+		if (!actContra || !contra || !reContra)
+			throw new Error("Datos invalidos");
+
+		const body = {
+			passwordAct: $("#password").val(),
+			passwordNew: $("#newPassword").val(),
 		}
-		click++
+		$(this).prop('disabled', true);
+		$.post("", body, function (des) {
+			if (des.resultado === 'Editada Contraseña') {
+				Toast.fire({ icon: 'success', title: 'Contraseña Actualizada' });
+				$("#perfil").click();
+				$("#profile-change-password input").val("")
+			}
+		}, "json")
+			.always(() => $(this).prop('disabled', false))
+			.fail((e) => {
+				Toast.fire({ icon: "error", title: e.responseJSON.msg || "Ha ocurrido un error." });
+				throw new Error(e.responseJSON.msg);
+			})
+
 	})
 
-	//Validacion de Existencia para la Cedula 
-	let val
-	function validarC(input, div, prefijo) {
-		val = prefijo.val() + "-" + input.val()
-		return new Promise((resolve, reject) => {
-			$.getJSON('', {
-				cedula: val,
-				validar: 'xd'
-			},
-				function (valid) {
-					if (valid.resultado === "Error") {
-						div.text("Error de Documento, " + valid.msj);
-						input.attr("style", "border-color: red;");
-						input.attr("style", "border-color: red; background-image: url(assets/img/Triangulo_exclamacion.png); background-repeat: no-repeat; background-position: right calc(0.375em + 0.1875rem) center; background-size: calc(0.75em + 0.375rem) calc(0.75em + 0.375rem);");
-						return reject(false);
-					} else {
-						div.text("");
-						return resolve(true);
-					}
-				}
-			)
+	async function validarCedulaBD(input, div, prefijo) {
+		let cedula = prefijo.val() + "-" + input.val()
+		let resultado = false;
+		await $.getJSON('', { cedula, validarCedula: '' }, function (valid) {
+			div.text("");
+			input.removeClass('input-error');
+			resultado = true;
+		}).fail(e => {
+			div.text(e.responseJSON.msg);
+			input.addClass('input-error')
+			resultado = false;
 		})
+		return resultado;
 	}
 
-	//Validacion de Existencia para Correo
-	function validarE(input, div) {
-		return new Promise((resolve, reject) => {
-			$.getJSON('', {
-				correo: input.val(),
-				validarE: 'lol'
-			},
-				function (valid) {
-					if (valid.resultado === "Error") {
-						div.text("Error de Correo, " + valid.msj);
-						input.attr("style", "border-color: red;");
-						input.attr("style", "border-color: red; background-image: url(assets/img/Triangulo_exclamacion.png); background-repeat: no-repeat; background-position: right calc(0.375em + 0.1875rem) center; background-size: calc(0.75em + 0.375rem) calc(0.75em + 0.375rem);");
-						return reject(false);
-					} else {
-						div.text("");
-						return resolve(true);
-					}
-				}
-			)
+	async function validarCorreoBD(input, div) {
+		let resultado = false;
+		await $.getJSON('', { correo: input.val(), validarCorreo: '' }, function (res) {
+			div.text("");
+			input.removeClass('input-error')
+			resultado = true;
+		}).fail(e => {
+			div.text(e.responseJSON.msg);
+			input.addClass('input-error')
+			resultado = false;
 		})
+		return resultado;
+	}
+
+	function loading() {
+		let xhr = new window.XMLHttpRequest();
+		$('#displayProgreso').show();
+		xhr.upload.addEventListener("progress", function (event) {
+			if (event.lengthComputable) {
+				let porcentaje = parseInt((event.loaded / event.total * 100), 10);
+				$('#progressBar').data("aria-valuenow", porcentaje)
+				$('#progressBar').css("width", porcentaje + '%')
+				$('#progressBar').html(porcentaje + '%')
+			}
+		}, false)
+		return xhr;
 	}
 
 })
--- bin/controlador/perfilControlador.php
+++ bin/controlador/perfilControlador.php
@@ -1,68 +1,70 @@
-<?php 
+<?php
 
-  use component\initcomponents as initcomponents;
-  use component\header as header;
-  use component\menuLateral as menuLateral;
-  use modelo\perfil as perfil;
+use component\initcomponents as initcomponents;
+use component\header as header;
+use component\menuLateral as menuLateral;
+use modelo\perfil as perfil;
 
 
 
-  $objModel = new perfil();
-  
-  if(!isset($_SESSION['nivel'])){
-    die('<script> window.location = "?url=login" </script>');
-  }
+$objModel = new perfil();
 
-  if(isset($_POST['notificacion'])) {
-    $objModel->getNotificacion();
-  }
+if (!isset($_SESSION['nivel'])) {
+  die('<script> window.location = "?url=login" </script>');
+}
 
-  $permisos = $objModel->getPermisosRol($_SESSION['nivel']);
-  
-  if(isset($_SESSION['cedula']) && isset($_POST['mostrar'])) {
-    $objModel->mostrarDatos($_SESSION['cedula']);
-  }
+if (isset($_POST['notificacion'])) {
+  $objModel->getNotificacion();
+}
 
-  if(isset($_POST['usuarios'], $_POST['lista'])){
-    $objModel->mostrarUsuarios();
-  }
+$permisos = $objModel->getPermisosRol($_SESSION['nivel']);
 
-  if(isset($_POST['password'], $_POST['validarContraseña'])){
-    $objModel->getValidarContraseña($_POST['password'], $_SESSION['cedula']);
-  }
+if (isset($_SESSION['cedula']) && isset($_POST['mostrar'])) {
+  $data = $objModel->mostrarDatos($_SESSION['cedula']);
+  die(json_encode($data));
+}
 
-  if (isset($_POST['nombre'], $_POST['apellido'], $_POST['cedula'], $_POST['email'], $_SESSION['cedula'])) {
+if (isset($_POST['usuarios'], $_POST['lista'])) {
+  $data = $objModel->mostrarUsuarios();
+  die(json_encode($data));
+}
 
-    if(isset($_POST['borrar']))
-      $objModel->getEditar('', $_POST['nombre'], $_POST['apellido'], $_POST['cedula'], $_POST['email'], $_SESSION['cedula'], $_POST['borrar']);
-    
-    if(isset($_FILES['foto']))
-      $objModel->getEditar($_FILES['foto'], $_POST['nombre'], $_POST['apellido'], $_POST['cedula'], $_POST['email'], $_SESSION['cedula']);
+if (isset($_POST['password'], $_POST['validarContraseña'])) {
+  $data = $objModel->getValidarContraseña($_POST['password'], $_SESSION['cedula']);
+  die(json_encode($data));
+}
 
-  }
+if (isset($_POST['nombre'], $_POST['apellido'], $_POST['cedula'], $_POST['email'], $_SESSION['cedula'])) {
 
-  if(isset($_SESSION['cedula']) && isset($_POST['passwordAct']) && isset($_POST['passwordNew']) && isset($_POST['passwordNewR'])) {
-    
-    $objModel->getCambioContra($_SESSION['cedula'], $_POST['passwordAct'], $_POST['passwordNew'], $_POST['passwordNewR']);
-  }
+  $data = "";
+  if (isset($_POST['borrar']))
+    $data = $objModel->getEditar('', $_POST['nombre'], $_POST['apellido'], $_POST['cedula'], $_POST['email'], $_SESSION['cedula'], $_POST['borrar']);
 
-  if (isset($_GET['validar'])) {
-		$res = $objModel->getValidarC($_GET['cedula'], $_SESSION['cedula']);
-		die(json_encode($res));
-	}
+  if (isset($_FILES['foto']))
+    $data = $objModel->getEditar($_FILES['foto'], $_POST['nombre'], $_POST['apellido'], $_POST['cedula'], $_POST['email'], $_SESSION['cedula']);
 
-	if (isset($_GET['validarE'])) {
-		$res = $objModel->getValidarE($_GET['correo'], $_SESSION['cedula']);
-		die(json_encode($res));
-	}
+  die(json_encode($data));
+}
 
-  $VarComp = new initcomponents();
-  $header = new header();
-  $menu = new menuLateral($permisos);
-  
-  if(file_exists("vista/interno/perfilVista.php")){
-    require_once("vista/interno/perfilVista.php");
-  } 
-  
+if (isset($_SESSION['cedula'], $_POST['passwordAct'], $_POST['passwordNew'])) {
+  $data = $objModel->getCambioContra($_SESSION['cedula'], $_POST['passwordAct'], $_POST['passwordNew']);
+  die(json_encode($data));
+}
 
-  ?>
+if (isset($_GET['validarCedula'], $_GET["cedula"])) {
+  $res = $objModel->getValidarCedula($_GET['cedula'], $_SESSION['cedula']);
+  die(json_encode($res));
+}
+
+if (isset($_GET['validarCorreo'])) {
+  $res = $objModel->getValidarCorreo($_GET['correo'], $_SESSION['cedula']);
+  die(json_encode($res));
+}
+
+$VarComp = new initcomponents();
+$header = new header();
+$menu = new menuLateral($permisos);
+
+if (file_exists("vista/interno/perfilVista.php")) {
+  require_once("vista/interno/perfilVista.php");
+}
