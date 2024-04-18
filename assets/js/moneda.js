4f1edabb8b89bd833fd47b95965189cab1df18c7
Victor Aparicio <apariciovictor862@gmail.com>
2024-04-17 21:16:02 -0400

    moneda listo, style btn editados

--- assets/js/moneda.js
+++ assets/js/moneda.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-	let tabla2
+	let tablaM
 
 
 	//Consulta de Permisos
@@ -14,7 +14,7 @@ $(document).ready(function () {
 		eliminarPermiso = (typeof permisos.Eliminar === 'undefined') ? 'disabled' : '';
 		$('#agregarMoneda, #agregarCambio').attr(registrarPermiso, '');
 	});
-	let cambio, fecha
+	let cambio, fecha, timeout
 	function mostrar(bitacora = false) {
 		$.ajax({
 			type: "POST",
@@ -23,67 +23,83 @@ $(document).ready(function () {
 			data: { datos: 'Moneda', bitacora },
 			success(data) {
 				data.forEach(row => {
-					cambio = (row.cambio == null) ? "" : row.cambio
+					cambio = (row.fecha == null) ? "" : row.cambio
 					fecha = (row.fecha == null) ? "" : row.fecha
-					tabla2 += `
+					tablaM += `
 					
 						<tr>
 							<td>${row.nombre} </td>
 							<td>${cambio} </td>
 							<td>${fecha} </td>
 							<td class="d-flex justify-content-center">
-							<button type="button" ${editarPermiso} class="btn btn-primary history mx-2" id="${row.id_moneda}" data-bs-toggle="modal" data-bs-target="#editHistory"><i class="bi bi-clock-history"></i></button>
-							<button type="button" ${editarPermiso} class="btn btn-success update mx-2" id="${row.id_moneda}" data-bs-toggle="modal" data-bs-target="#editModal"><i class="bi bi-pencil"></i></button>
+							<button type="button" ${editarPermiso} class="btn btn-view history mx-2" id="${row.id_moneda}" data-bs-toggle="modal" data-bs-target="#editHistory"><i class="bi bi-clock-history"></i></button>
+							<button type="button" ${editarPermiso} class="btn btn-registrar update mx-2" id="${row.id_moneda}" data-bs-toggle="modal" data-bs-target="#editModal"><i class="bi bi-pencil"></i></button>
 							<button type="button" ${eliminarPermiso} class="btn btn-danger delete mx-2" id="${row.id_moneda}" data-bs-toggle="modal" data-bs-target="#deleteModal"><i class="bi bi-trash3"></i></button>
 								</td>
 						</tr>`;
 				});
-				$('#tbody1').html(tabla2);
-				tabla2 = $('#tabla2').DataTable({
+				$('#tbody1').html(tablaM);
+				tablaM = $('#tablaM').DataTable({
 					responsive: true,
 				});
 
+			},
+			error(e) {
+				Toast.fire({ icon: "error", title: e.responseJSON.msg || "Ha ocurrido un error.", showCloseButton: true })
+				throw new Error(e.responseJSON.msg);
 			}
 		})
 	}
 
 	let click = 0;
-	setInterval(() => { click = 0; }, 1500);
+	//setInterval(() => { click = 0; }, 1500);
 
-	let moneda, name;
-	$('#moneda').keyup(() => { validarNombre($('#moneda'), $('#ms'), "Error de moneda") });
-	$('#registrar').click((e) => {
+	let name;
+	$('#moneda').keyup((e) => {
+		if (e.which === 13) return clearTimeout(timeout);
+		let valid = validarNombre($('#moneda'), $('#ms'), "Error de moneda,")
+		clearTimeout(timeout)
+		timeout = setTimeout(function () {
+			if (valid) { validarM($('#moneda'), $('#ms')) }
+		}, 700)
+	});
+	$('#registrarM').submit((e) => {
 		e.preventDefault();
-		if (click >= 1) throw new Error('Spam de clicks');
-
 		if (typeof permisos.Registrar === 'undefined') {
-			Toast.fire({ icon: 'error', title: 'No tienes permisos para esta acción.' });
+			Toast.fire({ icon: 'error', title: 'No tienes permisos para esta acción.', showCloseButton: true });
 			throw new Error('Permiso denegado.');
 		}
 
-
-
-		moneda = validarNombre($('#moneda'), $('#ms'), "Error de moneda");
-
+		let moneda = validarNombre($('#moneda'), $('#ms'), "Error de moneda");
 		if (moneda) {
-			name = $('#moneda').val();
+			validarM($('#moneda'), $('#ms')).then(() => {
+				$("#registrar").prop('disabled', true);
 
-			$.ajax({
-				type: 'POST',
-				url: '',
-				dataType: 'JSON',
-				data: { moneda: 'registrar', name },
-				success(data) {
-					console.log(tabla)
-					tabla2.destroy();
-					$('#cerrarR').click();
-					mostrar();
-					Toast.fire({ icon: 'success', title: 'Moneda registrado' });
-				}
+				name = $('#moneda').val();
+
+				$.ajax({
+					type: 'POST',
+					url: '',
+					dataType: 'JSON',
+					data: { moneda: 'xd', name },
+					success(data) {
+						console.log(data);
+						tablaM.destroy();
+						$('#cerrarMonR').click();
+						mostrar();
+						Toast.fire({ icon: 'success', title: 'Moneda registrada', showCloseButton: true });
+					},
+					error(e) {
+						Toast.fire({ icon: "error", title: e.responseJSON.msg || "Ha ocurrido un error.", showCloseButton: true })
+						throw new Error(e.responseJSON.msg);
+					},
+					complete() {
+						$("#registrar").prop('disabled', false);
+					}
+				})
 			})
 
 		}
-		click++
 	})
 
 	let id;
@@ -102,37 +118,51 @@ $(document).ready(function () {
 		})
 	})
 
-	$('#editMon').keyup(() => { validarNombre($('#editMon'), $('#ms2'), "Error de moneda") });
+	$('#editMon').keyup((e) => {
+		if (e.which === 13) return clearTimeout(timeout);
+		let valid = validarNombre($('#editMon'), $('#ms2'), "Error de moneda")
+		clearTimeout(timeout)
+		timeout = setTimeout(function () {
+			if (valid) { validarM($('#editMon'), $('#ms2'), id) }
+		}, 700)
+	});
 
-	$('#editar').click((e) => {
+	$('#editarMon').submit((e) => {
 		e.preventDefault();
-		if (click >= 1) throw new Error('Spam de clicks');
 
 		if (typeof permisos.Editar === 'undefined') {
-			Toast.fire({ icon: 'error', title: 'No tienes permisos para esta acción.' });
+			Toast.fire({ icon: 'error', title: 'No tienes permisos para esta acción.', showCloseButton: true });
 			throw new Error('Permiso denegado.');
 		}
 		let editN = validarNombre($('#editMon'), $('#ms2'), "Error de moneda");
 
 		if (editN) {
-			nameEdit = $('#editMon').val();
+			validarM($('#editMon'), $('#ms2'), id).then(() => {
 
-			$.ajax({
-				type: 'POST',
-				url: '',
-				dataType: 'JSON',
-				data: { nameEdit, id },
-				success(data) {
-					console.log(data)
-					console.log(tabla)
-					tabla2.destroy();
-					$('#cerrarA').click();
-					mostrar();
-					Toast.fire({ icon: 'success', title: 'Moneda actualizada' });
-				}
+				$("#editar").prop('disabled', true);
+				nameEdit = $('#editMon').val();
+
+				$.ajax({
+					type: 'POST',
+					url: '',
+					dataType: 'JSON',
+					data: { nameEdit, id },
+					success(data) {
+						tablaM.destroy();
+						$('#cerrarMonA').click();
+						mostrar();
+						Toast.fire({ icon: 'success', title: 'Moneda actualizada', showCloseButton: true  });
+					},
+					error(e) {
+						Toast.fire({ icon: "error", title: e.responseJSON.msg || "Ha ocurrido un error.", showCloseButton: true })
+						throw new Error(e.responseJSON.msg);
+					},
+					complete() {
+						$("#editar").prop('disabled', false);
+					}
+				})
 			})
 		}
-		click++
 	})
 
 	$(document).on('click', '.delete', function () {
@@ -142,38 +172,45 @@ $(document).ready(function () {
 
 	$('#eliminar').click((e) => {
 		e.preventDefault()
-		if (click >= 1) throw new Error('Spam de clicks');
+
 		if (typeof permisos.Eliminar === 'undefined') {
-			Toast.fire({ icon: 'error', title: 'No tienes permisos para esta acción.' });
+			Toast.fire({ icon: 'error', title: 'No tienes permisos para esta acción.', showCloseButton: true  });
 			throw new Error('Permiso denegado.');
 		}
+		$("#eliminar").prop('disabled', true);
+		validarM($('#editMon'), $('#ms2'), id).then(() => {
 
-		$.ajax({
-			type: "POST",
-			url: '',
-			dataType: 'json',
-			data: {
-				delete: 'eliminar',
-				id
-			},
-			success(data) {
-				console.log(data)
-				console.log(tabla)
-				tabla2.destroy();
-				$('#cerrar').click();
-				mostrar();
-				Toast.fire({ icon: 'success', title: 'Moneda eliminada' });
-			}
+			$.ajax({
+				type: "POST",
+				url: '',
+				dataType: 'json',
+				data: {
+					delete: 'eliminar',
+					id
+				},
+				success(data) {
+					tablaM.destroy();
+					$('#cerrarMonE').click();
+					mostrar();
+					Toast.fire({ icon: 'success', title: 'Moneda eliminada', showCloseButton: true  });
+				},
+				error(e) {
+					Toast.fire({ icon: "error", title: e.responseJSON.msg || "Ha ocurrido un error.", showCloseButton: true })
+					throw new Error(e.responseJSON.msg);
+				},
+				complete() {
+					$("#eliminar").prop('disabled', false);
+				}
+			})
 		})
-		click++
 	})
 
 
-	let tabla
-	let idHistory
+	let tabla, idHistory
+
 	async function rellenar(idHistory) {
-		
-		if(typeof tabla !== 'undefined') tabla.destroy();
+
+		if (typeof tabla !== 'undefined') tabla.destroy();
 		$('#selectMoneda').val(idHistory)
 		await $.ajax({
 			type: "POST",
@@ -181,18 +218,18 @@ $(document).ready(function () {
 			dataType: 'json',
 			data: { mostrar: 'xd', idHistory },
 			success(angeles) {
-				
+
 				$("#nomMoneda").text(angeles[0]['nombre'])
 
 				angeles.forEach(row => {
 					if (row.cambio == null) return
-					
+
 					tabla += `
 						<tr>
 						<td>${row.cambio} </td>
 						<td>${row.fecha} </td>
 						<td class="d-flex justify-content-center">
-						<button type="button" ${editarPermiso} class="btn btn-success editar mx-2" id="${row.id_cambio}" data-bs-toggle="modal" data-bs-target="#editarModal"><i class="bi bi-pencil"></i></button>
+						<button type="button" ${editarPermiso} class="btn btn-registrar editar mx-2" id="${row.id_cambio}" data-bs-toggle="modal" data-bs-target="#editarModal"><i class="bi bi-pencil"></i></button>
 						<button type="button" ${eliminarPermiso} class="btn btn-danger borrar mx-2" id="${row.id_cambio}" data-bs-toggle="modal" data-bs-target="#delModal"><i class="bi bi-trash3"></i></button>
 						</td>
 						</tr>`;
@@ -202,6 +239,10 @@ $(document).ready(function () {
 					responsive: true,
 					"order": [[1, "desc"]]
 				});
+			},
+			error(e) {
+				Toast.fire({ icon: "error", title: e.responseJSON.msg || "Ha ocurrido un error.", showCloseButton: true })
+				throw new Error(e.responseJSON.msg);
 			}
 		})
 		// mostrar()
@@ -210,12 +251,15 @@ $(document).ready(function () {
 	}
 
 	$(document).on('click', '.history', async function () {
-
+		$(".history").prop('disabled', true);
 		idHistory = this.id;
-
 		await rellenar(idHistory)
-		;
 	})
+
+	$('#editHistory').on('hidden.bs.modal', function () {
+		// Aquí va el código que deseas ejecutar cuando se cierra el modal
+		$(".history").prop('disabled', false);
+	});
 
 
 	selectMoneda();
@@ -233,13 +277,13 @@ $(document).ready(function () {
 					option += `<option value="${row.id_moneda}">${row.nombre}</option>`;
 				})
 				$('.selectM').html(option);
-					
-				
+
+
 			}
 		})
 	}
 	let resultado
-	$(document).on('click', '#agregarMoneda', async function (){
+	$(document).on('click', '#agregarMoneda', async function () {
 		await selectMoneda();
 		$('#selectMoneda').val(idHistory)
 	})
@@ -249,43 +293,49 @@ $(document).ready(function () {
 	$("#selectMoneda").change(function () {
 		select = validarSelect($("#selectMoneda"), $("#error"), "Error de Tipo de Moneda,")
 	})
-	$("#cambio").keyup(() => { validarNumero($("#cambio"), $("#error"), "Error de Valor de Moneda,") });
+	$("#cambio").keyup((e) => {
+		if (e.which === 13) return
+		validarNumero($("#cambio"), $("#error"), "Error de Valor de Moneda,")
+	});
 
 
-	$("#enviar").click((e) => {
-		e.preventDefault()
-		if (click >= 1) throw new Error('Spam de clicks');
-
+	$("#registrarC").submit((e) => {
+		e.preventDefault();
 		if (typeof permisos.Registrar === 'undefined') {
-			Toast.fire({ icon: 'error', title: 'No tienes permisos para esta acción.' });
+			Toast.fire({ icon: 'error', title: 'No tienes permisos para esta acción.', showCloseButton: true  });
 			throw new Error('Permiso denegado.');
 		}
 
-		vcambio = validarNumero($("#cambio"), $("#error"), "Error de Valor de Moneda,");
-		select = validarSelect($("#selectMoneda"), $("#error"), "Error de Tipo de Moneda,");
+		let vcambio = validarNumero($("#cambio"), $("#error"), "Error de Valor de Moneda,");
+		let select = validarSelect($("#selectMoneda"), $("#error"), "Error de Tipo de Moneda,");
 
 		if (select && vcambio) {
+			$("#enviar").prop('disabled', true);
 			$.ajax({
 
 				type: "POST",
 				url: "",
 				dataType: "json",
 				data: {
-
 					cambio: $("#cambio").val(),
 					tipo: $("#selectMoneda").val()
-
 				},
 				success(data) {
-					tabla2.destroy();
+					tablaM.destroy();
 					$("#close").click();
-					Toast.fire({ icon: 'success', title: 'Tipo de cambio registrado' })
+					Toast.fire({ icon: 'success', title: 'Cambio de Moneda Registrado', showCloseButton: true  })
 					mostrar()
 					rellenar(idHistory)
+				},
+				error(e) {
+					Toast.fire({ icon: "error", title: e.responseJSON.msg || "Ha ocurrido un error.", showCloseButton: true })
+					throw new Error(e.responseJSON.msg);
+				},
+				complete() {
+					$("#enviar").prop('disabled', false);
 				}
 			})
 		}
-		click++
 
 	})
 
@@ -295,9 +345,9 @@ $(document).ready(function () {
 	})
 	$("#delete").click((e) => {
 		e.preventDefault()
-		if (click >= 1) throw new Error('Spam de clicks');
+		$("#delete").prop('disabled', true);
 		if (typeof permisos.Eliminar === 'undefined') {
-			Toast.fire({ icon: 'error', title: 'No tienes permisos para esta acción.' });
+			Toast.fire({ icon: 'error', title: 'No tienes permisos para esta acción.', showCloseButton: true  });
 			throw new Error('Permiso denegado.');
 		}
 		$.ajax({
@@ -309,17 +359,22 @@ $(document).ready(function () {
 				id
 			},
 			success(consul) {
-				tabla2.destroy();
+				tablaM.destroy();
 				$("#closeDel").click();
-				Toast.fire({ icon: 'error', title: 'Tipo de moneda eliminado' })
+				Toast.fire({ icon: 'error', title: 'Cambio de moneda eliminado', showCloseButton: true  })
 				mostrar()
 				rellenar(idHistory)
-
+			},
+			error(e) {
+				Toast.fire({ icon: "error", title: e.responseJSON.msg || "Ha ocurrido un error.", showCloseButton: true })
+				throw new Error(e.responseJSON.msg);
+			},
+			complete() {
+				$("#delete").prop('disabled', false);
 			}
 		})
 
 
-		click++
 
 	})
 
@@ -340,6 +395,9 @@ $(document).ready(function () {
 				$("#monedaEdit").val(uni[0].moneda);
 				$("#cambioEdit").val(uni[0].cambio);
 
+			}, error(e) {
+				Toast.fire({ icon: "error", title: e.responseJSON.msg || "Ha ocurrido un error.", showCloseButton: true })
+				throw new Error(e.responseJSON.msg);
 			}
 
 		})
@@ -350,49 +408,89 @@ $(document).ready(function () {
 	$("#cambioEdit").keyup(() => { validarNumero($("#cambioEdit"), $("#error2"), "Error de Valor de Moneda,") });
 
 	let etipo, ecambio;
-	$("#enviarEdit").click((e) => {
+	$("#editarCambio").submit((e) => {
 		e.preventDefault()
-		if (click >= 1) throw new Error('Spam de clicks');
 
 		if (typeof permisos.Editar === 'undefined') {
-			Toast.fire({ icon: 'error', title: 'No tienes permisos para esta acción.' });
+			Toast.fire({ icon: 'error', title: 'No tienes permisos para esta acción.', showCloseButton: true  });
 			throw new Error('Permiso denegado.');
 		}
 
 		etipo = validarSelect($("#monedaEdit"), $("#error2"), "Error de Tipo de Moneda,");
 		ecambio = validarNumero($("#cambioEdit"), $("#error2"), "Error de Valor de Moneda,");
 
-		$.ajax({
-
-			type: "POST",
-			url: "",
-			dataType: "json",
-			data: {
-				cambioEdit: $("#cambioEdit").val(),
-				tipoEdit: $("#monedaEdit").val(),
-				unico
-
-			},
-			success(data) {
-				tabla2.destroy();
-				if (etipo == true && ecambio == true) {
-
+		if (etipo && ecambio) {
+			$("#enviarEdit").prop('disabled', true);
+			$.ajax({
+				type: "POST",
+				url: "",
+				dataType: "json",
+				data: {
+					cambioEdit: $("#cambioEdit").val(),
+					tipoEdit: $("#monedaEdit").val(),
+					unico
+				},
+				success(data) {
+					tablaM.destroy();
 					$("#closeEdit").click();
-					Toast.fire({ icon: 'success', title: 'Tipo de cambio registrado' })
-					// rellenar();
-
-				} else {
-					e.preventDefault()
+					Toast.fire({ icon: 'success', title: 'Cambio de Moneda editado' , showCloseButton: true })
+					mostrar()
+					rellenar(idHistory)
+				},
+				error(e) {
+					Toast.fire({ icon: "error", title: e.responseJSON.msg || "Ha ocurrido un error.", showCloseButton: true })
+					throw new Error(e.responseJSON.msg);
+				},
+				complete() {
+					$("#enviarEdit").prop('disabled', false);
 				}
-				mostrar()
-				rellenar(idHistory)
-
-			}
-		})
-
-		click++
+			})
+		}
 
 	})
+
+	// Vacio de Modales para Moneda
+	$(document).on('click', '#cerrarMonA', function () {
+		$('#editModal p').text(" ");
+		$("#editModal input, select").removeClass('input-error')
+	})
+	$(document).on('click', '#cerrarMonR', function () {
+		$('#registrarMoneda p').text(" ");
+		$("#registrarMoneda input, select").removeClass('input-error')
+	})
+	$(document).on('click', '#cerrarMonE', function () {
+		$("#deleteModal p").text(" ");
+	})
+
+	// Vacio de Modales para Cambio
+	$(document).on('click', '#close', function () {
+		$('#editarModal p').text(" ");
+		$("#editarModal input, select").removeClass('input-error')
+	})
+	$(document).on('click', '#closeEdit', function () {
+		$('#registrarModal p').text(" ");
+		$("#registrarModal input, select").removeClass('input-error')
+	})
+	$(document).on('click', '#closeDel', function () {
+		$("#delModal p").text(" ");
+	})
+
+	// Validacion de Moneda
+	async function validarM(input, div, id = " ") {
+		input = (input.val() == "undefined") ? " " : input
+		let resultado = false
+		await $.getJSON('', { nomMoneda: input.val(), id, validar: 'xd' }, function (res) {
+			div.text("");
+			input.removeClass('input-error')
+			resultado = true;
+		}).fail(e => {
+			div.text("Error de Moneda, " + e.responseJSON.msg);
+			input.addClass('input-error')
+			resultado = false;
+
+		})
+		return resultado
+	}
 
 
 
