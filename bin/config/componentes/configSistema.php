845d7d41ad6c152f9584ea603a652a5372bce750
Ricardo Parra <ricardo.prxr16@gmail.com>
2024-04-17 21:48:06 -0400

    feat: datos globales migrados al .env

--- bin/config/componentes/configSistema.php
+++ bin/config/componentes/configSistema.php
@@ -1,72 +1,74 @@
 <?php
-    
-    namespace config\componentes;
 
-    define("_URL_", $_ENV["URL"]);
-    define("_BD_" , $_ENV['DB']);
-    define("_PASS_" , $_ENV['DB_PASS']);
-    define("_USER_" , $_ENV['DB_USER']);
-    define("_LOCAL_" , $_ENV['DB_HOST']);
-    define("_PORT_", $_ENV['DB_PORT']);
-    define("DIRECTORY" , "bin/controlador/");
-    define("MODEL", "modelo/");
-    define("CONTROLADOR", "Controlador.php");
-    define("_SMTP", $_ENV['SMTP']);
-    define("_SMTP_USER", $_ENV['SMTP_USER']);
-    define("_SMTP_PASS", $_ENV['SMTP_PASS']);
-    define("KEY", $_ENV['KEY']);
-    define("CIPHER", $_ENV['CIPHER']);
-    define("IV", $_ENV['IV']);
+namespace config\componentes;
+
+use Dotenv\Dotenv as Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__, "../../../.env");
+$dotenv->load();
+
+define("_URL_", $_ENV["URL"]);
+define("_BD_", $_ENV['DB']);
+define("_PASS_", $_ENV['DB_PASS']);
+define("_USER_", $_ENV['DB_USER']);
+define("_LOCAL_", $_ENV['DB_HOST']);
+define("_PORT_", $_ENV['DB_PORT']);
+define("DIRECTORY", "bin/controlador/");
+define("MODEL", "modelo/");
+define("CONTROLADOR", "Controlador.php");
+define("_SMTP", $_ENV['SMTP']);
+define("_SMTP_USER", $_ENV['SMTP_USER']);
+define("_SMTP_PASS", $_ENV['SMTP_PASS']);
+define("KEY", $_ENV['KEY']);
+define("CIPHER", $_ENV['CIPHER']);
+define("IV", $_ENV['IV']);
 
 
-    class configSistema{
-    	
-    public function _int(){
-       if(!file_exists("bin/controlador/frontControlador.php")){
-        return "Error configSistema!";
+class configSistema {
 
-      }
-    }
-    
-    public function _URL_(){
-      return _URL_;
-    }
-    public function _BD_(){
-      return _BD_;
-    }
-    public function _PASS_(){
-      return _PASS_;
-    }
-    public function _PORT_(){
-      return _PORT_;
-    }
-    public function _USER_(){
-      return _USER_;
-    }
-    public function _LOCAL_(){
-      return _LOCAL_;
-    }
-    public function _Dir_(){
-      return DIRECTORY;
-    }
-    public function _MODEL_(){
-      return MODEL;
-    }
-    public function _Control_(){
-      return CONTROLADOR;
-    }
-    
-    public function KEY(){
-      return KEY;
-    }
-
-    public function CIPHER(){
-      return CIPHER;
-    }
-
-    public function IV(){
-      return IV;
+  public function _int() {
+    if (!file_exists("bin/controlador/frontControlador.php")) {
+      return "Error configSistema!";
     }
   }
 
-?>
+  public function _URL_() {
+    return _URL_;
+  }
+  public function _BD_() {
+    return _BD_;
+  }
+  public function _PASS_() {
+    return _PASS_;
+  }
+  public function _PORT_() {
+    return _PORT_;
+  }
+  public function _USER_() {
+    return _USER_;
+  }
+  public function _LOCAL_() {
+    return _LOCAL_;
+  }
+  public function _Dir_() {
+    return DIRECTORY;
+  }
+  public function _MODEL_() {
+    return MODEL;
+  }
+  public function _Control_() {
+    return CONTROLADOR;
+  }
+
+  public function KEY() {
+    return KEY;
+  }
+
+  public function CIPHER() {
+    return CIPHER;
+  }
+
+  public function IV() {
+    return IV;
+  }
+}
