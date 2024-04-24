ca248fed54034aee81a9c533b7ce5b55758d7972
enmanuel <enmanuel.josue.torres13@gmail.com>
2024-04-24 03:22:02 -0400

    feat: change query ventas

--- bin/modelo/ventas.php
+++ bin/modelo/ventas.php
@@ -25,16 +25,18 @@
          SELECT v.num_fact, v.monto_fact , pe.cedula AS cedula, pe.nombres AS nombre , v.fecha, (SELECT CONCAT(v.monto_dolares, ' ', m.nombre) FROM moneda m WHERE UPPER(m.nombre) = 'DOLAR') AS total_divisa
          FROM venta v 
          INNER JOIN venta_personal vpe ON vpe.num_fact = v.num_fact 
+         INNER JOIN pagos_recibidos pr ON pr.num_fact = v.num_fact
          LEFT JOIN personal pe ON pe.cedula = vpe.cedula 
-         WHERE v.status = 1
+         WHERE v.status = 1 AND pr.status = 1
 
          UNION ALL
 
          SELECT v.num_fact, v.monto_fact,  pa.ced_pac AS cedula, pa.nombre AS nombre , v.fecha, (SELECT CONCAT(v.monto_dolares, ' ', m.nombre) FROM moneda m WHERE UPPER(m.nombre) = 'DOLAR') AS total_divisa
          FROM venta v 
          INNER JOIN venta_pacientes vpa ON vpa.num_fact = v.num_fact 
+         INNER JOIN pagos_recibidos pr ON pr.num_fact = v.num_fact
          LEFT JOIN pacientes pa ON pa.ced_pac = vpa.ced_pac 
-         WHERE v.status = 1";
+         WHERE v.status = 1 AND pr.status = 1;";
 
          $new = $this->con->prepare($query);
          $new->execute();
