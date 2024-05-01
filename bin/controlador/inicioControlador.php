f03e5f12d324fc0bcaea4343e101177e46fdc6bd
Ricardo Parra <ricardo.pxr16@gmail.com>
2024-04-30 22:17:32 -0400

    fix: inicio sin carrito ni funciones en el modelo

--- bin/controlador/inicioControlador.php
+++ bin/controlador/inicioControlador.php
@@ -1,27 +1,14 @@
 <?php
 
-	use component\initcomponents as initcomponents;
-	use component\tienda as tienda;
-	use component\footerInicio as footerInicio;
-	use modelo\inicio as inicio;
+use component\initcomponents as initcomponents;
+use component\tienda as tienda;
+use component\footerInicio as footerInicio;
+use modelo\inicio as inicio;
 
-	$model = new inicio();
+$model = new inicio();
 
-	if (isset($_POST['mostraC'])) {
-	 	$model->mostrarCatalogo();
-	}
+$VarComp = new initcomponents();
+$tiendaComp = new tienda();
+$footer = new footerInicio();
+require "vista/inicio/inicioVista.php";
 
-	if (isset($_POST['mostraProductos']) && isset($_POST['id'])) {
-		$model->rellenarDatos($_POST['id']);
-	}
-	
-	if(isset($_POST['mostrarCategorias'])){
-		$model->mostrarCategorias();
-	}
-
-	$VarComp = new initcomponents();	
-	$tiendaComp = new tienda();
-	$footer= new footerInicio(); 
-	require "vista/inicio/inicioVista.php";	
-
-?>
