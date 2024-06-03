c29e0058f336c3b0b163fadc3a59babba12f0870
Ricardo Parra <ricardo.pxr16@gmail.com>
2024-06-03 19:14:24 -0400

    feat: inventario adaptado a JWToken

--- bin/controlador/inventarioControlador.php
+++ bin/controlador/inventarioControlador.php
@@ -5,16 +5,19 @@ use component\initcomponents as initcomponents;
 use component\header as header;
 use component\menuLateral as menuLateral;
 use modelo\inventario as inventario;
+use utils\JWTService;
 
-if(!isset($_SESSION['nivel'])) {
+$JWToken = JWTService::validateSession();
+if (!isset($_SESSION['nivel']) && !$JWToken) {
     die('<script> window.location = "?url=login" </script>');
 }
 
+$nivel = (isset($_SESSION['nivel'])) ? $_SESSION['nivel'] : $JWToken->nivel;
 $objModel = new inventario();
-$permisos = $objModel->getPermisosRol($_SESSION['nivel']);
+$permisos = $objModel->getPermisosRol($nivel);
 $permiso = $permisos['Inventario'];
 
-if(!isset($permiso["Consultar"])) {
+if (!isset($permiso["Consultar"])) {
     die('<script> window.location = "?url=login" </script>');
 }
 
@@ -22,21 +25,22 @@ if(!isset($permiso["Consultar"])) {
 //$objModel->getNotificacion();
 //}
 
-if(isset($_POST['getPermisos'], $permiso["Consultar"])) {
+if (isset($_POST['getPermisos'], $permiso["Consultar"])) {
     die(json_encode($permiso));
 }
 
-if(isset($_POST['mostrar'])) {
+if (isset($_POST['mostrar'])) {
     $res = $objModel->mostrarInventario($_POST['bitacora']);
     die(json_encode($res));
 }
+
 
 
 $VarComp = new initcomponents();
 $header = new header();
 $menu = new menuLateral($permisos);
 
-if(file_exists("vista/interno/productos/inventarioVista.php")) {
+if (file_exists("vista/interno/productos/inventarioVista.php")) {
     require_once("vista/interno/productos/inventarioVista.php");
 } else {
     die("La vista no existe.");
